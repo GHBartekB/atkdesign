c0e131f6d9f99900085684417094d75d3016f8e5
Bartek Brychcy <bbrychcy@onet.eu>
2021-02-11 11:37:08 +0100

    add and config  html-webpack-plugin

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,15 +1,20 @@
 const path = require('path');
-const { CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const  HtmlWebpackPlugin  = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'dev.js',
+        filename: '[name]-[contenthash:6].js',
         path: path.resolve(__dirname, '../', 'build' ),
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/template.html'
+        })
     ],
 
 }
