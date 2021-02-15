7673b9973fd2bff89ae364238b0fc15a3ea4fa85
Bartek Brychcy <bbrychcy@onet.eu>
2021-02-15 11:42:31 +0100

    change dev config in webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const  HtmlWebpackPlugin  = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'js/[name]-[contenthash:6].js',
+        filename: 'js/[name].js',
         path: path.resolve(__dirname, '../', 'build' ),
     },
     devServer: {
@@ -19,11 +18,11 @@ module.exports = {
        rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                use: ['style-loader', 'css-loader'],
             },
             { 
                 test: /\.(sass|scss)$/,
-                use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader',]
+                use: ['style-loader','css-loader','sass-loader',]
             }
         ]
     },
@@ -34,9 +33,7 @@ module.exports = {
             filename: 'index.html',
             template: './src/template/template.html'
         }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        })
+
     ],
 
 }
