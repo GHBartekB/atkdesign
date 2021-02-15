ffae3b1e98bb8ef4da0ca8f45b78059113e680ab
Bartek Brychcy <bbrychcy@onet.eu>
2021-02-15 11:27:29 +0100

    change prod config in webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const  HtmlWebpackPlugin  = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'prod.js',
-        path: path.resolve(__dirname, '../', 'build')
-    }
-} 
+        filename: 'js/[name]-[contenthash:6].js',
+        path: path.resolve(__dirname, '../', 'build' ),
+    },
+    module: {
+       rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            { 
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader',]
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/template/template.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name]-[contenthash:6].css',
+        })
+    ],
+
+}
